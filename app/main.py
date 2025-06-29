bf82f5060824f50dd1c6e083536ccad147a49157
rmalldt <rmall.dt@gmail.com>
2025-06-29 19:40:03 +0100

    Setup db connection and updated routes functions

--- app/main.py
+++ app/main.py
@@ -1,128 +1,106 @@
-from random import randrange
-from typing import Optional
+import os
+import time
 from fastapi import FastAPI, HTTPException, Response, status
 from pydantic import BaseModel
 import uvicorn
+from dotenv import load_dotenv
+import psycopg2
+from psycopg2.extras import RealDictCursor
 
-# FastAPI ASGI app
+load_dotenv()
+
 app = FastAPI()
 
 
-# Class using pydantic BaseModel
 class Post(BaseModel):
     title: str
     content: str
     published: bool = True
-    rating: Optional[int] = None
 
 
-# Mock DB
-db_posts = [
-    {
-        "title": "Post 1",
-        "content": "Content of the post 1 ",
-        "rating": 5,
-        "id": 1,
-    },
-    {
-        "title": "Post 2",
-        "content": "Content of the post 2",
-        "rating": 3,
-        "id": 2,
-    },
-]
+while True:
+    try:
+        conn = psycopg2.connect(
+            host=os.getenv("DB_HOST"),
+            database=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            cursor_factory=RealDictCursor,
+        )
+        cursor = conn.cursor()
+        print(f"Database connection successful.")
+        break
+    except Exception as error:
+        print(f"Failed to connect database. Error: {error}")
+        time.sleep(2)
 
 
-# Find Post
-def find_post(id: int) -> dict | None:
-    for post in db_posts:
-        if post["id"] == id:
-            return post
-
-
-# Find Post, same as above but using generator
-def find_post_gen(id: int):
-    res = next((post for post in db_posts if post["id"] == id), None)
-    return res
-
-
-# Find Post index
-def find_post_index(id: int) -> int | None:
-    for i, post in enumerate(db_posts):
-        if post["id"] == id:
-            return i
-
-
-# GET /
 @app.get("/")
 async def root():
-    # FastAPI automatically converts the Python dict to json
-    return {"message": "Hello there!"}
+    return {"message": "A CRUD API for testing."}
 
 
-# GET /posts
 @app.get("/posts")
 async def get_posts():
-    # FastAPI automatically converts the Python array to json
-    return {"data": db_posts}
+    cursor.execute("""SELECT * FROM post LIMIT 100;""")
+    posts = cursor.fetchall()
+    return {"success": True, "data": posts}
 
 
 # POST /posts
 @app.post("/posts")
 async def create_posts(post: Post, response: Response):
-    post_dict = post.model_dump()
-    post_dict["id"] = randrange(1, 1000)
-    db_posts.append(post_dict)
+    cursor.execute(
+        """INSERT INTO post (title, content) VALUES (%s, %s) RETURNING *;""",
+        (post.title, post.content),
+    )
+    new_post = cursor.fetchone()
+    conn.commit()  # finalize and push the changes to the DB
     response.status_code = status.HTTP_201_CREATED
-    return {"data": post_dict}
+    return {"success": True, "data": new_post}
 
 
-# GET /posts/:id
-# Note: The extracted param values are always String type.
-# Always annotate the function param type and FastAPI will
-# automatically validate (check that type can be converted)
-# and parse the received type into the requried to type.
-# E.g., str is converted to int below.
 @app.get("/posts/{id}")
 async def get_post(id: int):
-    post = find_post_gen(id)
+    cursor.execute("""SELECT * FROM post WHERE id = %s;""", (str(id),))
+    post = cursor.fetchone()
     if not post:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"post with id: {id} not found",
+            detail=f"Post with id: {id} not found",
         )
-    return {"data": post}
+    return {"success": True, "data": post}
 
 
-# DELETE /posts
 @app.delete("/posts/{id}")
 async def delete_post(id: int):
-    index = find_post_index(id)
-    if index == None:
+    cursor.execute("""DELETE FROM post WHERE id = %s RETURNING *;""", (str(id),))
+    deleted_post = cursor.fetchone()
+    conn.commit()
+    if not deleted_post:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"post with id: {id} not found",
+            detail=f"Post with id: {id} not found",
         )
-    db_posts.pop(index)
 
-    # Send only status code as response.
-    # Delete request doesn't require response data to be sent.
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
-# PUT /posts/:id
 @app.put("/posts/{id}")
 async def update_post(id: int, post: Post):
-    index = find_post_index(id)
-    if index == None:
+    cursor.execute(
+        """UPDATE post SET title = %s, content = %s WHERE id = %s RETURNING *;""",
+        (post.title, post.content, str(id)),
+    )
+    updated_post = cursor.fetchone()
+    conn.commit()
+
+    if not updated_post:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"post with id: {id} not found",
+            detail=f"Post with id: {id} not found",
         )
-    post_dict = post.model_dump()
-    post_dict["id"] = id
-    db_posts[index] = post_dict
-    return {"data": post_dict}
+    return {"result": True, "data": updated_post}
 
 
 if __name__ == "__main__":
