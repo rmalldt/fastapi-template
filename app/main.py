8789f9e1259b8b163e20f2bf27af2a2501c300c4
rmalldt <rmall.dt@gmail.com>
2025-07-06 01:31:15 +0100

    Configure CORS and add requirements.txt

--- app/main.py
+++ app/main.py
@@ -3,11 +3,24 @@ from fastapi import FastAPI
 from . import models
 from .database import engine
 from .routers import auth, user, post, vote
+from fastapi.middleware.cors import CORSMiddleware
 
 
-models.Base.metadata.create_all(bind=engine)
+# Now the migration is managed with Alembic
+# models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
