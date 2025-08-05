6302b37c1a74e006ff354fe0431c31ea2d54c5a9
rmalldt <rmall.dt@gmail.com>
2025-08-05 01:44:46 +0100

    Add auth path test

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,7 +1,9 @@
 from fastapi.testclient import TestClient
+import jwt
 from app.main import app
-from app.schemas import UserOut
-from .testdb import session, client
+from app.schemas import Token, UserOut
+from app.config import settings
+from .testdb import session, client, test_user
 
 
 def test_register_user(client):
@@ -13,3 +15,21 @@ def test_register_user(client):
     new_user = UserOut(**res.json())
     assert new_user.email == "test123@gmail.com"
     assert res.status_code == 201
+
+
+def test_login_user(client, test_user):
+    res = client.post(
+        "/auth/login",
+        data={"username": test_user["email"], "password": test_user["password"]},
+    )
+
+    login_res = Token(**res.json())
+
+    payload = jwt.decode(
+        login_res.access_token, settings.secret_key, algorithms=[settings.algorithm]
+    )
+
+    id = int(payload.get("sub"))
+    assert id == test_user["id"]
+    assert login_res.token_type == "bearer"
+    assert res.status_code == 200
--- tests/testdb.py
+++ tests/testdb.py
@@ -1,6 +1,6 @@
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker
 import pytest
 from app.main import app
 from app.config import settings
@@ -19,6 +19,7 @@ TestSessionLocal = sessionmaker(autoflush=False, bind=engine)
 
 @pytest.fixture()
 def session():
+    print("Create test session")
     Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
     db = TestSessionLocal()
@@ -30,6 +31,8 @@ def session():
 
 @pytest.fixture()
 def client(session):
+    print("Create test client")
+
     def override_get_db():
         try:
             yield session
@@ -38,3 +41,16 @@ def client(session):
 
     app.dependency_overrides[get_session] = override_get_db
     yield TestClient(app)
+
+
+@pytest.fixture
+def test_user(client):
+    print("Create test user")
+    user_data = {"email": "test123@gmail.com", "password": "password123"}
+    res = client.post("/auth/register/", json=user_data)
+
+    assert res.status_code == 201
+
+    new_user = res.json()
+    new_user["password"] = user_data["password"]
+    return new_user
